f44395f49396c7f1577aa6780019fd4a8c9620e5
mondalankan2007-a11y <mondalankan2007@gmail.com>
2026-09-06 18:16:59 +0530

    feat: GET/events

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,23 @@
 const express = require("express")
 const app = express()
 
+const events = [
+  {
+    id: "1",
+    title: "Tekron",
+    description: "This is a tekron event",
+  },
+  {
+    id: "2",
+    title: "Blood Donation Camp",
+    description: "This is a blood donation camp",
+  },
+];
+
+app.get("/events",(req,res)=>{
+    res.json(events)
+})
+
 app.listen(3000,()=>{
     console.log(`Server started on port 3000`)
 })
